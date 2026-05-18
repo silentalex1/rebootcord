38318404b014725f0783306289ea3fa8b8e86745
Zal <101911964+silentalex1@users.noreply.github.com>
2026-05-18 16:14:12 -0400

    Create dashbaord.js

---
+++ dashbaord.js
@@ -0,0 +1,9 @@
+function uploadBot() {
+    alert("Bot upload system coming soon");
+}
+
+fetch('/api/stats')
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById('active-users').textContent = data.activeUsers;
+    });
