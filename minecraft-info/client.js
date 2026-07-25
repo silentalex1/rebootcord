75b151f7ec517b84a7cae8cd4d61477d667ac669
Zal <101911964+silentalex1@users.noreply.github.com>
2026-07-25 04:43:58 -0400

    Add files via upload

---
+++ minecraft-info/client.js
@@ -0,0 +1,5 @@
+const body = document.getElementById('mcClientBody');
+const comingSoon = document.createElement('div');
+comingSoon.className = 'mc-coming-soon';
+comingSoon.textContent = 'coming soon';
+body.appendChild(comingSoon);
